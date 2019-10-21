15886ad00a22685ab23511d3ac26ca600301212b
yong <yongsan89@naver.com>
2019-10-21 17:58:54 +0900

    .

--- src/main/java/team/sports/matching/BettingController.java
+++ src/main/java/team/sports/matching/BettingController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BattingController {
+public class BettingController {
 
 	
 	@RequestMapping("/Team/Matching/Betting.do")
