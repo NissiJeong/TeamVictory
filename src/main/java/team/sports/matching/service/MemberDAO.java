d3e3fd676939b463d613b279bc32773a99c05006
정닛시 <jnissi92@gmail.com>
2019-10-22 23:05:37 +0900

    2019-10-22 controller,jsp페이지 수정

--- src/main/java/team/sports/matching/service/MemberDAO.java
+++ src/main/java/team/sports/matching/service/MemberDAO.java
@@ -1,5 +1,6 @@
 package team.sports.matching.service;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -22,5 +23,23 @@ public class MemberDAO {
 		return template.insert("memberRegi", map);
 		
 	}///memberRegi
-	
+	///팀 만들기
+	public int teamInsert(Map map) {
+		return template.insert("teamInsert",map);
+	}
+	//팀멤버 테이블에 팀 insert
+	public int insertTeamMember(Map map) {
+		return template.insert("teamMemberInsert",map);
+	}
+	public int deleteTeam(Map map) {
+		return template.delete("deleteTeam", map);
+	}
+	//id와 같은 팀명 가져오기 
+	public List<Map> selectTeamName(String id){
+		return template.selectList("selectTeamName",id); 
+	}
+	//ajax로 고른 팀 데이터 가져오기
+	public Map selectTeam(Map map) {
+		return template.selectOne("selectTeam", map);
+	}/////
 }/////class
