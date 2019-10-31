6f8f54c90b3a45c3e1471ec59bde82adde89e5eb
박승용 <intotheakira@gmail.com>
2019-10-31 13:02:17 +0900

    문의 보내기 수정중

---
+++ src/main/java/team/sports/matching/service/LetterAction.java
@@ -0,0 +1,71 @@
+package team.sports.matching.service;
+
+public class LetterAction {
+	String id;
+	String title;
+	String content;
+	String email;
+	String telephone;
+	String name;
+	/*
+	LetterBean letter;
+	public String execute() {
+		letter = new LetterBean();
+		send_id =(String)ActionContext.getContext().getSession().get("id)");
+		
+		letter.setId(id);
+		letter.setTitle(title);
+		letter.setContent(content);
+		letter.setSend_id(send_id);
+		System.out.println(id);
+		
+		MVCProcess mvcp = MVCProcess.getInstance();
+		mvcp.insertLetter(letter);
+		return "success";
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LetterBean getLetter() {
+		return letter;
+	}
+	public void setLetter(LetterBean letter) {
+		this.letter = letter;
+	}
+	*/
+	
+
+}
