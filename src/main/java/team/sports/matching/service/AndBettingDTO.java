bd5d84295517a4dc386b42e3dbc91b4b4bdcc3b9
songsick <songsig2@naver.com>
2019-11-26 00:08:51 +0900

    1125asdf

---
+++ src/main/java/team/sports/matching/service/AndBettingDTO.java
@@ -0,0 +1,63 @@
+package team.sports.matching.service;
+
+public class AndBettingDTO {
+	private String no;
+	private String gamedate;
+	private String time;
+	private String hometeam;
+	private String homelogo;
+	private String awayteam;
+	private String awaylogo;
+	private String stadium;
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getGamedate() {
+		return gamedate;
+	}
+	public void setGamedate(String gamedate) {
+		this.gamedate = gamedate;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getHometeam() {
+		return hometeam;
+	}
+	public void setHometeam(String hometeam) {
+		this.hometeam = hometeam;
+	}
+	public String getHomelogo() {
+		return homelogo;
+	}
+	public void setHomelogo(String homelogo) {
+		this.homelogo = homelogo;
+	}
+	public String getAwayteam() {
+		return awayteam;
+	}
+	public void setAwayteam(String awayteam) {
+		this.awayteam = awayteam;
+	}
+	public String getAwaylogo() {
+		return awaylogo;
+	}
+	public void setAwaylogo(String awaylogo) {
+		this.awaylogo = awaylogo;
+	}
+	public String getStadium() {
+		return stadium;
+	}
+	public void setStadium(String stadium) {
+		this.stadium = stadium;
+	}
+	
+	
+	
+}
